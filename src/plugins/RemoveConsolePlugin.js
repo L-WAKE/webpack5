884ab942dc97f53bf2252c25fedc712715e6be15
liujin <2454243839@qq.com>
2023-04-04 17:58:23 +0800

    自定义插件

--- src/plugins/RemoveConsolePlugin.js
+++ src/plugins/RemoveConsolePlugin.js
@@ -1,3 +1,12 @@
+// 自定义插件
+
+/*
+    (1.编写一个类，实现 apply(compiler) 方法。
+    (2.在 apply 方法中，使用 compiler.hooks 对象绑定一个或多个钩子函数。
+    (3.钩子函数会在Webpack执行到相应的阶段时被调用，并被传入一个 compilation 对象和其它参数。
+    (4.钩子函数处理compilation对象，可以修改、添加、删除生成的资源文件。
+    (5.插件类导出，添加到Webpack配置文件中的plugins数组中。
+*/
 class RemoveConsolePlugin {
   apply(compiler) {
     compiler.hooks.emit.tap("RemoveConsolePlugin", (compilation) => {
